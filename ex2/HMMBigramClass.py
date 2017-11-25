2d40152cf344fd820ac4c45f5b1ed24513bedd5f
omer <omerbh1@gmail.com>
2017-11-25 21:26:30 +0200

    c almost done

--- ex2/HMMBigramClass.py
+++ ex2/HMMBigramClass.py
@@ -1,3 +1,4 @@
+import numpy as np
 class HMMBigramTaggerCLass:
     def __init__(self):
         self.trainset = {}
@@ -39,12 +40,24 @@ class HMMBigramTaggerCLass:
         self.setDics(train_sentences)
         self.countWords(train_sentences)
 
-#
+
+    def setPorbMatrix(self,Sk,sentence):
+        number_of_possible_tags = len(Sk[1])
+        n = len(sentence)
+        probMatrix = np.zeros((number_of_possible_tags,n))
+        for i in range(n):
+            for j in range(number_of_possible_tags):
+                probMatrix[i,j] = 1 # todo this isnot correct
+
+        return probMatrix
+
     def tag(self,sentence):
         n = len(sentence)
         Sk = []
         Sk.append(["*"])
-        for i in range(1,n-1):
+        for i in range(1,n):
             Sk.append(list(self.possibleTags.keys()))
         Sk.append(["*"])
-        print(Sk)
+        probMatrix = self.setPorbMatrix(Sk,sentence)
+
+
