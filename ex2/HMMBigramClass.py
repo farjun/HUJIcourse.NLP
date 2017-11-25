2a213ac835d7e30658deccc4aa79a580540fdfb3
omer <omerbh1@gmail.com>
2017-11-25 21:56:56 +0200

    c almost done

--- ex2/HMMBigramClass.py
+++ ex2/HMMBigramClass.py
@@ -18,7 +18,7 @@ class HMMBigramTaggerCLass:
                 # all pairs of tags
                 self.counter_dic[(sentence[i][1], sentence[i + 1][1])] = 0
                 # all tags
-                self.counter_dic[(sentence[i][1], "*")] = 0
+                self.counter_dic[sentence[i][1]] = 0
                 self.possibleTags[sentence[i][1]] = 1
 
     def countWords(self, train_sentences):
@@ -34,30 +34,28 @@ class HMMBigramTaggerCLass:
                 # all pairs of tags
                 self.counter_dic[(sentence[i][1], sentence[i + 1][1])] += 1
                 # all tags
-                self.counter_dic[(sentence[i][1], "*")] += 1
+                self.counter_dic[sentence[i][1]] += 1
 
     def train(self, train_sentences):
         self.setDics(train_sentences)
         self.countWords(train_sentences)
 
 
-    def setPorbMatrix(self,Sk,sentence):
-        number_of_possible_tags = len(Sk[1])
+    def setPorbMatrix(self,number_of_possible_tags,sentence):
         n = len(sentence)
-        probMatrix = np.zeros((number_of_possible_tags,n))
-        for i in range(n):
-            for j in range(number_of_possible_tags):
-                probMatrix[i,j] = 1 # todo this isnot correct
+        return np.zeros((number_of_possible_tags,n))
 
-        return probMatrix
 
     def tag(self,sentence):
         n = len(sentence)
         Sk = []
+        list_of_possible_tags = list(self.possibleTags.keys())
+
         Sk.append(["*"])
         for i in range(1,n):
-            Sk.append(list(self.possibleTags.keys()))
+            Sk.append(list_of_possible_tags.copy())
         Sk.append(["*"])
-        probMatrix = self.setPorbMatrix(Sk,sentence)
+
+        probMatrix = self.setPorbMatrix(n,sentence)
 
 
