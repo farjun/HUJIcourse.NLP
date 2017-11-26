0fa033bd60c0abf4df7de32b4da5df89d67acf67
omer <omerbh1@gmail.com>
2017-11-26 19:15:50 +0200

    c almost done

--- ex2/HMMBigramClass.py
+++ ex2/HMMBigramClass.py
@@ -45,6 +45,38 @@ class HMMBigramTagger:
         else:
             self._word_to_tag_count[word][tag] += 1
 
+
+
+    def findMaxProbabilityFromLastColum(self, probability_matrix_column, word, possible_prev_tags, cur_tag):
+        tag_probabilities = []
+
+        emission = self._word_to_tag_count[word][cur_tag]
+
+        for j in range(len(possible_prev_tags)):
+
+            prev_tag = possible_prev_tags[j]
+            p = self._tag_to_next_tag_count[prev_tag][cur_tag]/ self._tags_count[prev_tag]
+            pi = probability_matrix_column[j]
+
+            tag_probabilities.append(p*emission*pi)
+        return max(tag_probabilities)
+
+    def setPorbMatrix(self, Sk, sentence):
+        sentence_length = len(sentence)
+        number_of_tags = len(Sk)
+        probabilityMAtrix = np.zeros((number_of_tags, sentence_length))
+
+        # set probability of Start to 1 in all rows
+        probabilityMAtrix[:0] = 1
+        # set probability of Stop to 1 in all rows
+        probabilityMAtrix[:-1] = 1
+
+        for i in range(1,sentence_length):
+            for j in range(number_of_tags):
+                probabilityMAtrix[i,j] = self.findMaxProbabilityFromLastColum(probabilityMAtrix[i:],sentence[i],Sk[i],Sk[i][j])
+
+        return probabilityMAtrix
+
     def _getPossibleTags(self):
         return list(self._tags_count.keys())
 
@@ -61,22 +93,10 @@ class HMMBigramTagger:
         n = len(sentence)
         Sk = []
         list_of_possible_tags = self._getPossibleTags()
-
+        # set Sk (tags)
         Sk.append(["*"])
         for i in range(1, n):
             Sk.append(list_of_possible_tags[:])
-        Sk.append(["*"])
-        probMatrix = self.setPorbMatrix(n, sentence)
 
-    def setPorbMatrix(self, Sk, sentence):
-        sentence_length = len(sentence)
-        number_of_tags = len(Sk)
-        probabilityMAtrix = np.zeros((number_of_tags, sentence_length))
-        # set probability of Start to 1 in all rows
-        probabilityMAtrix[:0] = 1
-        # set probability of Stop to 1 in all rows
-        probabilityMAtrix[:-1] = 1
-
-        for i in range(1, sentence_length):
-            for j in range(number_of_tags):
-                probabilityMAtrix[i, j] = 0
+        probMatrix = self.setPorbMatrix(Sk, sentence)
+        print(probMatrix)
--- ex2/ex2Test.py
+++ ex2/ex2Test.py
@@ -1,10 +1,7 @@
-import ex2.ex2Sol as ex2Sol
+from ex2.HMMBigramClass import *
+from ex2.ex2Sol import *
+words,test = getTaggedSentences()
+a = HMMBigramTagger()
+a.train(words)
+a.tag("the")
 
-
-
-if __name__ == '__main__':
-    # train , test = ex2Sol.getTaggedSentences()
-    # ret = ex2Sol.HMMbigramTagger(train_sentences=train,test_sentences=test)
-    for i in range(-1,10):
-        print(i)
-    pass
