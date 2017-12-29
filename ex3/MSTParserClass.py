6d7e8bc2e309190de9ae60f85dac680b63c2d64c
ori_yt <ori_yt@cs.huji.ac.il>
2017-12-29 15:50:10 +0200

    word bigram

--- ex3/MSTParserClass.py
+++ ex3/MSTParserClass.py
@@ -6,52 +6,58 @@ class MSTParser:
     def __init__(self):
         # conputes which tuples will have the value 1 in the sentence
         # for example - sentences_words_dic[sentence1][(word1,word2)] = 1 iff word1 word2 were in sentence1
-        self.vocabulary_dic = dict()
         self.sentences_words_dic = dict()
-        self.tag_dic = dict()
         self.weights = np.zeros(1)
+
+        # maps from a value to the feature index.
+        self.vocabulary_dic = dict()
+        self.tag_dic = {'ROOT': 0}
 
     # <editor-fold desc="Pre-Processing">
     def generateVocabulery(self, train: np.ndarray, test: np.ndarray) -> None:
-        index = self.addWordsToVocabulary(0, train)
-        self.addWordsToVocabulary(index, test)
+        wordIndex, tagIndex = self.addWords(len(self.vocabulary_dic), len(self.tag_dic), train)
+        self.addWords(wordIndex, tagIndex, test)
 
-    def addWordsToVocabulary(self, index, sentences):
+    def addWords(self, wordIndex, tagIndex, sentences):
         for i in range(sentences.size):
             sentence = sentences[i]
             nodes = sentence.nodes
             for j in range(1, len(nodes)):
                 word = nodes[j]['word']
+                tag = nodes[j]['tag']
                 if word not in self.vocabulary_dic:
-                    self.vocabulary_dic[word] = index
-                    index += 1
-        return index
+                    self.vocabulary_dic[word] = wordIndex
+                    wordIndex += 1
+                if tag not in self.tag_dic:
+                    self.tag_dic[tag] = tagIndex
+                    tagIndex += 1
+        return wordIndex, tagIndex
 
     # </editor-fold>
 
 
 
     def train(self, train_sentences):
-        for sentence in train_sentences:
-            self.sentences_words_dic[sentence] = dict()
+        pass
 
-            for i in range(len(sentence) - 1):
-                self.sentences_words_dic[sentence][(sentence[i], sentence[i + 1])] = 1
-                # todo put tags here too
-                # todo think of how to init the weights vector better
-                # for now we save the vocabulary
-                self.vocabulary_dic[sentence[i]] = 1
-            self.vocabulary_dic[sentence[len(sentence)]] = 1
-
-    def generateArcs(self, sentence)->[MSTAlgorithem.Arc]:
+    def generateArcs(self, sentence) -> [MSTAlgorithem.Arc]:
         totalIndexes = len(sentence)
         arcs = []
         for fromIndex in range(totalIndexes):
             for toIndex in range(totalIndexes):
                 if toIndex in sentence[fromIndex]['deps']:
-                    arcs.append(MSTAlgorithem.Arc(fromIndex,1,toIndex))
+                    arcs.append(MSTAlgorithem.Arc(fromIndex, 1, toIndex))
                 else:
-                    arcs.append(MSTAlgorithem.Arc(fromIndex,0,toIndex))
+                    arcs.append(MSTAlgorithem.Arc(fromIndex, 0, toIndex))
+
+
+    def getWordBigram(self, sentence, fromNode, toNode):
+        return 1 if toNode['address'] in fromNode['deps'] else 0
+
+    def getPOSBigram(self, sentence, fromNode, toNode):
+        return 1 if toNode['address'] in fromNode['deps'] else 0
+
+
 
     # <editor-fold desc="Getters & Setters">
     def get_feature(self, sentence, word1, word2):
