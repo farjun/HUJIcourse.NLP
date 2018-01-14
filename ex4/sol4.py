099e65a68b726e104c1270403976fecf32fb39d6
omer bh <omerbh1@gmail.com>
2018-01-14 18:07:32 +0200

    ex4

--- ex4/sol4.py
+++ ex4/sol4.py
@@ -1,15 +1,28 @@
 from ex4.IterativeExtractor import IterativeExtractor
 from ex4.TreeExtractor import TreeExtractor
 
-if __name__ == '__main__':
-    ie = IterativeExtractor("Donald Trump")
-    triplets = ie.process()
-    print(len(triplets))
-    ie = IterativeExtractor("Brad Pitt")
-    triplets = ie.process()
-    print(len(triplets))
-    ie = IterativeExtractor("Angelina Jolie")
-    triplets = ie.process()
-    print(len(triplets))
 
-    #
+
+
+if __name__ == '__main__':
+    # ie = IterativeExtractor("Donald Trump")
+    # triplets = ie.process()
+    # print(len(triplets))
+    # ie = IterativeExtractor("Brad Pitt")
+    # triplets = ie.process()
+    # print(len(triplets))
+    # ie = IterativeExtractor("Angelina Jolie")
+    # triplets = ie.process()
+    # print(len(triplets))
+
+    ie = TreeExtractor("Donald Trump")
+    triplets = ie.process()
+    print(triplets)
+    ie = TreeExtractor("Brad Pitt")
+    triplets = ie.process()
+    print(triplets)
+    ie = TreeExtractor("Angelina Jolie")
+    triplets = ie.process()
+    print(triplets)
+
+
