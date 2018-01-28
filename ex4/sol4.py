91203c28372138df0720d3f06e77c8d7a8d37305
OriYitzhaki <ori.yithzaki@mail.huji.ac.il>
2018-01-28 19:39:32 +0200

    just run me

--- ex4/sol4.py
+++ ex4/sol4.py
@@ -1,28 +1,32 @@
 from ex4.IterativeExtractor import IterativeExtractor
 from ex4.TreeExtractor import TreeExtractor
 
+import random
+
+def test_page(page_name):
+    # ie = IterativeExtractor(page_name)
+    ie = TreeExtractor(page_name)
+    triplets = ie.process()
+    test = triplets[:]
+    random.shuffle(test)
+    real_test = test[:10]
+    log = open("log"+page_name.replace(" ",""),mode='w')
+    for triple in real_test:
+        print("="*25+"Start"+"="*25)
+        print(triple)
+        log.write(str(triple)+'\n')
+        print("="*25+"End"+"="*25)
+    print(len(triplets))
 
 
+def main():
+    random.seed(1234)
+    page_name = "Donald Trump"
+    test_page(page_name)
+    page_name = "Brad Pitt"
+    test_page(page_name)
+    page_name = "Angelina Jolie"
+    test_page(page_name)
 
 if __name__ == '__main__':
-    # ie = IterativeExtractor("Donald Trump")
-    # triplets = ie.process()
-    # print(len(triplets))
-    # ie = IterativeExtractor("Brad Pitt")
-    # triplets = ie.process()
-    # print(len(triplets))
-    # ie = IterativeExtractor("Angelina Jolie")
-    # triplets = ie.process()
-    # print(len(triplets))
-
-    ie = TreeExtractor("Donald Trump")
-    triplets = ie.process()
-    print(triplets)
-    ie = TreeExtractor("Brad Pitt")
-    triplets = ie.process()
-    print(triplets)
-    ie = TreeExtractor("Angelina Jolie")
-    triplets = ie.process()
-    print(triplets)
-
-
+    main()
