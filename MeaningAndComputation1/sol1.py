45bc3695ffb7930be5f0f9f58b4ac3775e5dac37
ori_yt <ori_yt@cs.huji.ac.il>
2018-04-27 17:52:01 +0300

    MAC_EX1

--- MeaningAndComputation1/sol1.py
+++ MeaningAndComputation1/sol1.py
@@ -4,8 +4,16 @@ corpus_name = "corpus_ex1"
 freq_list_name = "freq_list_ex1"
 if __name__ == '__main__':
     corpus = open(corpus_name)
-    lines = [line for line in corpus]
-    print(len(lines))
-    freq_list = open(freq_list_name)
-    lines = [line for line in freq_list]
-    print(len(lines))
+    words = [line for line in corpus]
+    words = list(map(lambda w:w.replace("\n",""),words))
+
+    s = ""
+    for word in words:
+        if "<s>" in word:
+            s = ""
+            continue
+        elif "</s>" in word:
+            print(s)
+            continue
+        else:
+            s += word + " "
